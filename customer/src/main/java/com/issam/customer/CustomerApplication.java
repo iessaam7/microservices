184f0e26a3e891c496e3d39db8a5cd8f0a35b808
DESKTOP-S43QPJN\iSSAM <issam@sofy.ai>
2023-12-22 15:18:55 +0500

    Customer and Fraud Microservices added till lesson 12

---
+++ customer/src/main/java/com/issam/customer/CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.issam.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
---
+++ customer/src/main/java/com/issam/customer/CustomerService.java
@@ -0,0 +1,20 @@
+package com.issam.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        //todo check if email is valid
+        //todo check if email not taken
+        //todo check if fraudster
+        customerRepository.save(customer);
+        // todo: send notification
+    }
+}
