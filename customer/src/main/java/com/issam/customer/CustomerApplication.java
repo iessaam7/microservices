01378ea40cf8db7f081d198b73f8f87312813203
DESKTOP-S43QPJN\iSSAM <issam@sofy.ai>
2024-01-01 17:19:21 +0500

    Added Eureka Configuration for the microservices L18

--- customer/src/main/java/com/issam/customer/CustomerApplication.java
+++ customer/src/main/java/com/issam/customer/CustomerApplication.java
@@ -2,10 +2,12 @@ package com.issam.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.swing.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
