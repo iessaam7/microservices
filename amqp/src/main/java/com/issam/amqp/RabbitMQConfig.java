88becb46da59de5a6b290534358dd2e3e971059c
DESKTOP-S43QPJN\iSSAM <issam@sofy.ai>
2024-12-15 19:30:15 +0100

    Added RabbitMQMessageProducer L-49

--- amqp/src/main/java/com/issam/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/issam/amqp/RabbitMQConfig.java
@@ -7,10 +7,10 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties
+@Configuration
 @AllArgsConstructor
 public class RabbitMQConfig {
 
